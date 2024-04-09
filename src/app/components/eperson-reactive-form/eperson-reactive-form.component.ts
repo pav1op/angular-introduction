53fab51f9d1c71a1c533ef29a3dc8af80156eb5c
Pavlos P <pavl.pantel@gmail.com>
2024-04-09 17:43:26 +0300

    corrections from 12

--- src/app/components/eperson-reactive-form/eperson-reactive-form.component.ts
+++ src/app/components/eperson-reactive-form/eperson-reactive-form.component.ts
@@ -1,23 +1,28 @@
 import { Component, EventEmitter, Output } from '@angular/core';
-import { MatFormField } from '@angular/material/form-field'
-import { MatInputModule } from '@angular/material/input'
-import { MatSelectModule } from '@angular/material/select'
-import { MatButtonModule } from '@angular/material/button'
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { EPerson } from 'src/app/shared/interfaces/person';
-
 
 @Component({
   selector: 'app-eperson-reactive-form',
   standalone: true,
   imports: [
     ReactiveFormsModule,
-    MatFormField,
+    MatFormFieldModule,
     MatInputModule,
     MatSelectModule,
-    MatButtonModule],
+    MatButtonModule,
+  ],
   templateUrl: './eperson-reactive-form.component.html',
-  styleUrl: './eperson-reactive-form.component.css'
+  styleUrl: './eperson-reactive-form.component.css',
 })
 export class EpersonReactiveFormComponent {
   @Output() person = new EventEmitter<EPerson>();
@@ -29,12 +34,10 @@ export class EpersonReactiveFormComponent {
       Validators.required,
       Validators.pattern('^[0-9]*$'),
       Validators.min(18),
-      Validators.max(100)
+      Validators.max(100),
     ]),
-    email: new FormControl('',[
-      Validators.required, Validators.email
-    ]),
-    education: new FormControl('', Validators.required)
+    email: new FormControl('', [Validators.required, Validators.email]),
+    education: new FormControl('', Validators.required),
   });
 
   onSubmit(value: any) {
--- src/app/components/reactive-form-example/reactive-form-example.component.ts
+++ src/app/components/reactive-form-example/reactive-form-example.component.ts
@@ -1,17 +1,19 @@
 import { Component } from '@angular/core';
 import { EpersonReactiveFormComponent } from '../eperson-reactive-form/eperson-reactive-form.component';
-import { EPerson, Person } from 'src/app/shared/interfaces/person';
+import { EPerson } from 'src/app/shared/interfaces/person';
 import { PersonTableComponent } from '../person-table/person-table.component';
 import { SimpleDatatableComponent } from '../simple-datatable/simple-datatable.component';
 
 @Component({
   selector: 'app-reactive-form-example',
   standalone: true,
-  imports: [EpersonReactiveFormComponent,
-  PersonTableComponent,
-SimpleDatatableComponent],
+  imports: [
+    EpersonReactiveFormComponent,
+    PersonTableComponent,
+    SimpleDatatableComponent,
+  ],
   templateUrl: './reactive-form-example.component.html',
-  styleUrl: './reactive-form-example.component.css'
+  styleUrl: './reactive-form-example.component.css',
 })
 export class ReactiveFormExampleComponent {
   currentPerson: EPerson;
