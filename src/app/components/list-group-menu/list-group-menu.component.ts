fa1688da76bcb1a7f5cfc8bac7249fc29f7d2cc4
Pavlos P <pavl.pantel@gmail.com>
2024-04-23 19:19:03 +0300

    fun for nerds

--- src/app/components/list-group-menu/list-group-menu.component.ts
+++ src/app/components/list-group-menu/list-group-menu.component.ts
@@ -26,5 +26,6 @@ export class ListGroupMenuComponent {
       },
       {text: 'User Registration Example', routerLink: 'user-registration-example'},
       {text: 'Restricted Content Example', routerLink: 'restricted-content-example', },
+      {text: 'Fun for Nerds Game', routerLink: 'fun-for-nerds'},
   ];
 }
